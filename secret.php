532381d3703469db8b4d30c09d5b14e201b941d0
Parfait TOLEFO <level@Level>
2022-07-29 21:52:46 +0000

    Upluad

---
+++ secret.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Codes d'accès au serveur central de la NASA</title>
+</head>
+<body>
+<?php
+if (isset($_POST['name']) AND isset($_POST['mot_de_passe']) AND $_POST['name'] == "Merveille" AND $_POST['mot_de_passe'] ==
+"!jaden006") // Si le mot de passe est bon
+{
+// On affiche les codes
+?>
+<h1>Voici les codes d'accès :</h1>
+<p><strong> CSD{d0_Y0u4_B3sT3_W1th2_Y0u4_Sk&ll} </strong></p>
+
+<?php
+}
+else // Sinon, on affiche un message d'erreur
+{
+echo '<p>Mot de passe incorrect</p>';
+}
+?>
+</body>
+</html>
